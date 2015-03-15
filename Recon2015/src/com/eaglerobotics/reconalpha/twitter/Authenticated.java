20265d52dd4f5ad8f1c2bb120c061efd70248a24
Joe <jdog.titanmaster@gmail.com>
2015-03-15 13:43:40 -0700

    Revert "Initial"
    This reverts commit 3b228d8fba6e9d0013b911e357ec35595518c177.

--- Recon2015/src/com/eaglerobotics/reconalpha/twitter/Authenticated.java
+++
@@ -1,6 +0,0 @@
-package com.eaglerobotics.reconalpha.twitter;
-
-public class Authenticated {
-    String token_type;
-    String access_token;
-}
--- Recon2015/src/com/eaglerobotics/reconalpha/twitter/Twitter.java
+++
@@ -1,8 +0,0 @@
-package com.eaglerobotics.reconalpha.twitter;
-
-import java.util.ArrayList;
-
-//a collection of tweets
-public class Twitter extends ArrayList<Tweet> {
-     private static final long serialVersionUID = 1L;
-}
